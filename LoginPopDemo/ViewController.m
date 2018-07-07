cfdd8d5162985f1bfd3678e61da452a95fdeefd7
gychao <757237523@qq.com>
2018-07-07 11:18:54 +0800

    Initial Commit

---
+++ LoginPopDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LoginPopDemo
+//
+//  Created by ganyanchao on 2018/7/7.
+//  Copyright © 2018年 G.Y. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ LoginPopDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LoginPopDemo
+//
+//  Created by ganyanchao on 2018/7/7.
+//  Copyright © 2018年 G.Y. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
